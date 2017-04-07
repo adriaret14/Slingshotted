f41eb229fefb35b655e9ed3d764cfdb690a4198f
Adria Fret <adriafret1995@gmail.com>
2017-04-07 13:50:46 +0200

    Animacion de combate y remodelacion primer nivel

--- Assets/Scripts/ControlesJugador.cs
+++ Assets/Scripts/ControlesJugador.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Timers;
 
 public class ControlesJugador : MonoBehaviour {
 
@@ -9,24 +11,28 @@ public class ControlesJugador : MonoBehaviour {
     private Animator anim;
     private bool PlayerMoving;
     private Vector2 lastMove;
+    private bool canMove = true;
 
-	// Use this for initialization
-	void Start () {
+    private double timer = 0;
+    private double meleeCD = 0.4f;
+
+    // Use this for initialization
+    void Start () {
         anim = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update() {
 
         PlayerMoving = false;
         anim.SetFloat("DirY", 0);
         anim.SetFloat("DirX", 0);
 
         //Movimiento ejes
-
+        if (canMove) { 
         if (Input.GetKey(KeyCode.W))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1)* Speed * Time.deltaTime;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1) * Speed * Time.deltaTime;
             anim.SetFloat("DirY", 10);
             anim.SetInteger("LD", 1);
             PlayerMoving = true;
@@ -74,6 +80,7 @@ public class ControlesJugador : MonoBehaviour {
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(1, -1) * Speed * Time.deltaTime;
         }
+    }
 
 
         anim.SetBool("SeMueve", PlayerMoving);
@@ -86,6 +93,26 @@ public class ControlesJugador : MonoBehaviour {
             anim.SetFloat("DirY", 0);
             anim.SetFloat("DirX", 0);
             //anim.SetInteger("LD", 0);
+        }
+
+
+        //Ataque
+        if (Input.GetKey(KeyCode.Space) && canMove)
+        {
+            anim.SetBool("Slashing", true);
+            PlayerMoving = false;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0) * Speed * Time.deltaTime;
+            canMove = false;
+            timer = meleeCD;
+         }
+
+        if (timer > 0)
+            timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            canMove = true;
+            anim.SetBool("Slashing", false);
         }
     }
 }
